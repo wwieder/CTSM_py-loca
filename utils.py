da06557e2157680c24a77427517ce9b53cd5d9e6
wwieder <wwieder@ucar.edu>
2019-10-01 10:27:03 -0600

    Utilities for plotting

--- utils.py
+++ utils.py
@@ -2,11 +2,34 @@
 """copied from klindsay, https://github.com/klindsay28/CESM2_coup_carb_cycle_JAMES/blob/master/utils.py"""
 
 import re
-
+import cf_units as cf
 import cftime
 import numpy as np
+import xarray as xr
+from cartopy.util import add_cyclic_point
 
 #from xr_ds_ex import xr_ds_ex
+
+# generate annual means, weighted by days / month
+def weighted_annual_mean(array):
+    mon_day  = xr.DataArray(np.array([31,28,31,30,31,30,31,31,30,31,30,31]), dims=['month'])
+    mon_wgt  = mon_day/mon_day.sum()
+    return (array.rolling(time=12, center=False) # rolling
+            .construct("month") # construct the array
+            .isel(time=slice(11, None, 12)) # slice so that the first element is [1..12], second is [13..24]
+            .dot(mon_wgt, dims=["month"]))
+
+def change_units(ds, variable_str, variable_bounds_str, target_unit_str):
+    """ Applies unit conversion on an xarray DataArray """
+    orig_units = cf.Unit(ds[variable_str].attrs["units"])
+    target_units = cf.Unit(target_unit_str)
+    variable_in_new_units = xr.apply_ufunc(
+        orig_units.convert,
+        ds[variable_bounds_str],
+        target_units,
+        output_dtypes=[ds[variable_bounds_str].dtype],
+    )
+    return variable_in_new_units
 
 def clean_units(units):
     """replace some troublesome unit terms with acceptable replacements"""
@@ -89,3 +112,68 @@ def time_year_plus_frac(ds, time_name):
     tvals_days = cftime.date2num(tvals_cftime, 'days since 0000-01-01', calendar='noleap')
 
     return tvals_days / 365.0
+
+
+# add cyclic point
+def cyclic_dataarray(da, coord='lon'):
+    """ Add a cyclic coordinate point to a DataArray along a specified
+    named coordinate dimension.
+    >>> from xray import DataArray
+    >>> data = DataArray([[1, 2, 3], [4, 5, 6]],
+    ...                      coords={'x': [1, 2], 'y': range(3)},
+    ...                      dims=['x', 'y'])
+    >>> cd = cyclic_dataarray(data, 'y')
+    >>> print cd.data
+    array([[1, 2, 3, 1],
+           [4, 5, 6, 4]])
+    """
+    assert isinstance(da, xr.DataArray)
+
+    lon_idx = da.dims.index(coord)
+    cyclic_data, cyclic_coord = add_cyclic_point(da.values,
+                                                 coord=da.coords[coord],
+                                                 axis=lon_idx)
+
+    # Copy and add the cyclic coordinate and data
+    new_coords = dict(da.coords)
+    new_coords[coord] = cyclic_coord
+    new_values = cyclic_data
+
+    new_da = xr.DataArray(new_values, dims=da.dims, coords=new_coords)
+
+    # Copy the attributes for the re-constructed data and coords
+    for att, val in da.attrs.items():
+        new_da.attrs[att] = val
+    for c in da.coords:
+        for att in da.coords[c].attrs:
+            new_da.coords[c].attrs[att] = da.coords[c].attrs[att]
+
+    return new_da
+
+# as above, but for a dataset
+# doesn't work because dims are locked in a dataset
+'''
+def cyclic_dataset(ds, coord='lon'):
+    assert isinstance(ds, xr.Dataset)
+
+    lon_idx = ds.dims.index(coord)
+    cyclic_data, cyclic_coord = add_cyclic_point(ds.values,
+                                                 coord=ds.coords[coord],
+                                                 axis=lon_idx)
+
+    # Copy and add the cyclic coordinate and data
+    new_coords = dict(ds.coords)
+    new_coords[coord] = cyclic_coord
+    new_values = cyclic_data
+
+    new_ds = xr.DataSet(new_values, dims=ds.dims, coords=new_coords)
+
+    # Copy the attributes for the re-constructed data and coords
+    for att, val in ds.attrs.items():
+        new_ds.attrs[att] = val
+    for c in ds.coords:
+        for att in ds.coords[c].attrs:
+            new_ds.coords[c].attrs[att] = ds.coords[c].attrs[att]
+
+    return new_ds
+'''
